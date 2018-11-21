8da8e8dfdd5edb8b55adb417bf47b3964393b25f
Fredrik Lunde <fredrik.lunde3@gmail.com>
2018-11-21 11:25:25 +0100

    listing images

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -1,10 +1,12 @@
 import React from 'react';
 
 const ImageList = (props) => {
-    console.log(props.images);
+    const images = props.images.map(image => {
+        return <img alt='' key={image.id} src={image.urls.regular} />
+    });
     return (
     <div>
-        ImageList
+        {images}
     </div>
     );
 };
