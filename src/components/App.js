c11a8af4c201c1c0f515db73da01e63c4527791f
Fredrik Lunde <fredrik.lunde3@gmail.com>
2018-11-21 11:10:09 +0100

    printing result

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,11 @@ import Axios from 'axios';
 import SearchBar from './SearchBar';
 
 class App extends React.Component{
-    async onSearchSubmit(term){
+    state = {
+        images: []
+    };
+
+    onSearchSubmit = async (term) => {
         const response = await Axios
         .get('https://api.unsplash.com/search/photos', {
         params: {query: term },
@@ -11,13 +15,14 @@ class App extends React.Component{
                 Authorization: `Client-ID ${process.env.REACT_APP_UNSPLASH_CLIENT_ID}`
             }
         });
-        console.log(response.data.results)
+        this.setState({ images: response.data.results });
     }
 
     render(){
         return (
             <div className="ui container" style={{ marginTop: '10px' }}>
                 <SearchBar onSubmit={this.onSearchSubmit} />
+                Found {this.state.images.length} images
             </div>
         );
     };
